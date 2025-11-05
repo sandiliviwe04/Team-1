3784092dafc95c46358fb6b45815adcb1e2420d4
Kuhle Vulangile <kuhlevulangile@gmail.com>
2025-11-05 09:46:13 +0200

    Kuhle work

---
+++ operations-types.js
@@ -0,0 +1,10 @@
+const number1 = 60;
+const number2 = 40;
+str1 = number1.toString();
+sum = number1 + number2;
+product = number1 * number2;
+console.log(number1);
+console.log(number2);
+console.log(str1);
+console.log(sum);
+console.log(product);
